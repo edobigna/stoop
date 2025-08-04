2dbe307d811db9af5434bbdc5b9feb909d16d48a
edobigna <edo.bigna@gmail.com>
2025-08-04 11:05:56 +0200

    boh

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.33eirl5lseo"
+    "revision": "0.l7kjq88j9c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
